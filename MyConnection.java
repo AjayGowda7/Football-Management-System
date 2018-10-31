dab8bb67c2101a9a7b1d939342702d160e696437
AjayGowda7 <40961738+AjayGowda7@users.noreply.github.com>
2018-10-31 23:48:02 +0530

    Add files via upload

---
+++ MyConnection.java
@@ -0,0 +1,20 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+/**
+ *
+ * @author user
+ */
+public class MyConnection {
+    public static Connection getConnection(){
+        Connection con=null;
+        try{
+            Class.forName("com.mysql.jdbc.Driver");
+            con=DriverManager.getConnection("jdbc:mysql://localhost/java_login_register1","root","");
+        }catch(Exception ex){
+            System.out.println(ex.getMessage());
+        }
+        return con;
+    }
+}
